bb1c53c3e8888280033c2c268e0abd90cc8831f6
keshavsoft <nknnkumar@yahoo.com>
2025-01-18 17:06:45 +0530

    original deleted

--- src/assets/js/volt.js
+++ src/assets/js/volt.js
@@ -19,13 +19,6 @@
 const d = document;
 d.addEventListener("DOMContentLoaded", function(event) {
 
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: 'btn btn-primary me-3',
-            cancelButton: 'btn btn-gray'
-        },
-        buttonsStyling: false
-    });
 
     var themeSettingsEl = document.getElementById('theme-settings');
     var themeSettingsExpandEl = document.getElementById('theme-settings-expand');
