ba35d93e1a572bd3389a074e0880eef07f7f04ce
hemantshai <gaminihemanthsai@gmail.com>
2025-01-30 20:52:21 +0530

    Data

--- src/Js/AllFiles/Show/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/afterFetch/For200Res/BuildBsTable/ForColumns/EntryFile.js
+++ src/Js/AllFiles/Show/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/afterFetch/For200Res/BuildBsTable/ForColumns/EntryFile.js
@@ -1,11 +1,18 @@
 import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
+import { StartFunc as FileDataCount } from "./FileDataCount/entryFile.js";
+
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial")
+    let LocalFileDataCount = LocalColumns.find(element => element.field === "FileDataCount")
 
     if (LocalColumnOperateFine === undefined === false) {
         ColumnOperate({ inFindColumn: LocalColumnOperateFine });
+    };
+
+    if (LocalFileDataCount === undefined === false) {
+        FileDataCount({ inFindColumn: LocalFileDataCount });
     };
    
 };
