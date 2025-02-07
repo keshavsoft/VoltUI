7e9430be6fb748815065adfc02efb7ab61f44e74
keshavsoft <nknnkumar@yahoo.com>
2025-02-07 16:40:18 +0530

    BranchWise working

--- src/Js/Admin/AllBranches/AsArrayShowAll/entryFile.js
+++ src/Js/Admin/AllBranches/AsArrayShowAll/entryFile.js
@@ -2,7 +2,6 @@ import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 
 const StartFunc = () => {
-    
     let jVarLocalFromAdmin = true;
 
     if (jVarLocalFromAdmin) {
