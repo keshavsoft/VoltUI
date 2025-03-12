d276075c65f8a9e005be3d7628776e50d653ae98
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-03-12 12:22:53 +0530

    Branch names

---
+++ src/Js/UpComingDelivery/FormLoad/ToDataList/fromLocalStorage.js
@@ -0,0 +1,21 @@
+const StartFunc = async() => {
+    let jVarLocalBranchNames = await GetFetch();
+    jVarLocalBranchNames.forEach(element => {
+        jFLocalAddToItems({ inBranchName: element });
+    });
+};
+
+const jFLocalAddToItems = ({ inBranchName }) => {
+    // Corrected jQuery syntax for option creation and appending to select element
+    var option = $('<option></option>').val(inBranchName).text(inBranchName);
+    $('#Branch').append(option);
+};
+
+const GetFetch = async () => {
+    let LocalUrl = "/Custom/Cleaning/Dashboard/Admin/Reports/Common/BranchName";
+    let response = await fetch(LocalUrl);
+    let Data = await response.json();
+    return Data;
+};
+
+export { StartFunc };
