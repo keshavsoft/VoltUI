62c9477bb1bc7249fee8452c6653216428e440e7
sharath0036 <ksharathbabu2@gmail.com>
2025-03-18 10:09:39 +0530

    bugs

--- src/Js/OrderChecking/CheckOrderQrs/FormLoad/AddListeners/OrderInputId/keypress/Order/FetchFromFuncs/AfterFetch/Status500.js
+++ src/Js/OrderChecking/CheckOrderQrs/FormLoad/AddListeners/OrderInputId/keypress/Order/FetchFromFuncs/AfterFetch/Status500.js
@@ -1,9 +1,15 @@
-let StartFunc = () => {
-    swal.fire({
-        text: "No Data",
-        title: "Check",
-        icon: "error"
-    })
+let StartFunc = (inData) => {
+    if (!inData || inData.length === 0) {
+        Swal.fire({
+            text: "No Data",
+            title: "Check",
+            icon: "error"
+        });
+
+        $('#table').bootstrapTable("load", []);
+    } else {
+        $('#table').bootstrapTable("load", inData);
+    }
 };
 
 export { StartFunc };
