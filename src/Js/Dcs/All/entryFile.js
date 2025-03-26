89e28e9c1cf89d258ebaf292752d380c09e2f7b2
Anantha123761 <anatalakshmikurupudi2002@gmail.com>
2025-03-26 13:12:47 +0530

    Admin Ai VoltUI in Login Page is created

--- src/Js/Dcs/All/entryFile.js
+++ src/Js/Dcs/All/entryFile.js
@@ -1,9 +1,10 @@
 import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
-
+import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
 const StartFunc = () => {
     
-    let jVarLocalFromAdmin = true;
+   // let jVarLocalFromAdmin = true;
+   let jVarLocalFromAdmin = LoginModal({inSuccessFunc: StartFuncFormLoad})
 
     if (jVarLocalFromAdmin) {
         StartFuncFormLoad();
