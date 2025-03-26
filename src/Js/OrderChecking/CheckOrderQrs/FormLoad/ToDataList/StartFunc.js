84ff3ce264e15fbb10ad6267b23fe095b74422fb
Srivasu215 <sri.vasu215@gmail.com>
2025-03-26 12:05:42 +0530

    Js Changed

---
+++ src/Js/OrderChecking/CheckOrderQrs/FormLoad/ToDataList/StartFunc.js
@@ -0,0 +1,15 @@
+import { StartFunc as fromLocalStorage } from "./fromLocalStorage.js";
+
+const StartFunc = () => {
+  fromLocalStorage();
+  
+  if ("AllMastersData" in localStorage) {
+    let jVarLocalHeaderToDataListId =
+      document.getElementById("HeaderToDataListId");
+    jVarLocalHeaderToDataListId.click();
+  }
+};
+
+
+
+export { StartFunc };
