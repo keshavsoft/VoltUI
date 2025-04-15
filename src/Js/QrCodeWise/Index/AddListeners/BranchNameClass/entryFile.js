654dd9fb60976eb632d598aa6a77df7946838f25
sharath0036 <ksharathbabu2@gmail.com>
2025-04-15 11:28:02 +0530

    entrydet,washdet,withvoudet

---
+++ src/Js/QrCodeWise/Index/AddListeners/BranchNameClass/entryFile.js
@@ -0,0 +1,10 @@
+const StartFunc = () => {
+    let jVarLocalClassName = document.querySelectorAll("BranchNameClass");
+    for (let i = 0; i < jVarLocalClassName.length; i++) {
+        const element = jVarLocalClassName[i];
+        console.log(element);
+        
+        
+    }
+}
+export { StartFunc };
